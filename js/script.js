766228c572257ba99ec4f68e0cda1de2637cf95c
antoniostassi <stassiant@outlook.it>
2024-07-15 14:48:04 +0200

    JS | + FizzBuzz + Fizz + Buzz

--- js/script.js
+++ js/script.js
@@ -6,17 +6,23 @@ for (let i = 1; i <= 100; i++) {
     if ((i % 3 == 0) && (i%5 == 0)){
         // console.log("FizzBuzz");
         myNewDiv.classList.add("multiploentrambi");
+        myNewDiv.append("FizzBuzz");
     } else if (i%3 == 0){
         // console.log("Fizz");
         myNewDiv.classList.add("multiplo3");
+        myNewDiv.append("Fizz");
+
     } else if (i%5 == 0){
         // console.log("Buzz");
         myNewDiv.classList.add("multiplo5");
+        myNewDiv.append("Buzz");
+
     } else {
         // console.log(i);
+        myNewDiv.append(i);
+
     }
     myRow.append(myNewDiv);
-    myNewDiv.append(i);
     myNewDiv.classList.add("my-col", "square", "text-center" , "fw-bold");
 }
 
